130cb34d92ab89a6faddc43a1aacfad60964446c
Zezineto <zezinetosegundo@gmail.com>
2023-03-08 16:28:00 -0300

    atualizações gerais do código

--- cliente.py
+++ cliente.py
@@ -1,3 +1,5 @@
+from veiculo import listaCar, carro_por_placa, ALUGADO, DISPONIVEL
+
 class Cliente:
     def __init__(self, nome, iden):
         self.nome = nome
@@ -8,11 +10,12 @@ class Cliente:
         return f'\nNome: {self.nome} || id: {self.iden}\n'
 
 class Aluguel:
-    def __init__(self, nomec, datac, preçoc, atraso):
-        self.nomec = nomec
-        self.datac = datac
+    def __init__(self, cliente, placa, dataini, dataf,preçoc):
+        self.cliente = cliente
+        self.placa = placa
+        self.dataini = dataini
+        self.dataf = dataf
         self.preçoc = preçoc
-        self.atraso = atraso
     def __str__(self):
         return f''
     
@@ -23,6 +26,8 @@ c3 = Cliente('Isaac Clarke',745)
 c4 = Cliente('Nicole Clarke',746)#clientes criados para serem adicionados na lista
 listaCli.extend([c1,c2,c3,c4]) #função(???) para adicionar clientes na lista
 listaCli.sort(key=lambda x: x.nome) #usado para ordenar a lista em ordem alfabetica
+
+lista_alugueis = []
 
 #função para printar a lista de clientes
 def printCli():
@@ -37,4 +42,22 @@ def percorrer():
     for num in listaCli:
         if(num.iden == 746):
             print(num.nome,'\n')
+
+def consulta_preço(placa):
+    for carro in listaCar:
+        if(carro.placa==placa):
+            return carro.valorDia
+
+def consulta_cliente(ident):
+    for cli in listaCli:
+        if(cli.iden==ident):
+            return cli
         
+def alugar(placa_para_aluguel,idAlu,dataini,dataf):
+    placa = placa_para_aluguel
+    cliente = idAlu
+    valor = consulta_preço(placa)
+    aluguel = Aluguel(placa,cliente,dataini,dataf,valor)
+    carro = carro_por_placa(placa)
+    carro.alu = ALUGADO
+    return aluguel
